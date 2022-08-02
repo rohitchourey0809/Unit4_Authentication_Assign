a9a696e955c0debde911bdb485ae500c53e8e1b8
ROHIT CHOUREY <97465195+rohitchourey0809@users.noreply.github.com>
2022-08-02 13:40:10 +0530

    "Update"

--- src/index.js
+++ src/index.js
@@ -1,27 +1,22 @@
-const  express = require('express');
-const connect = require('./config/db');
-const usercontroller = require('./controller/user.controller')
-const productcontroller = require('./controller/product.controller')
-const {register,login} = require('./controller/auth.controller')
-
+const express = require("express");
+const connect = require("./config/db");
+// const usercontroller = require('./controller/user.controller')
+// const productcontroller = require('./controller/product.controller')
+// const {register,login} = require('./controller/auth.controller')
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-app.use("/userlink",usercontroller)
-app.post("/register",register)
-app.post("/login",login)
-app.use("/products",productcontroller)
+// app.use("/userlink",usercontroller)
+// app.post("/register",register)
+// app.post("/login",login)
+// app.use("/products",productcontroller)
 
-
-
-app.listen(5000,async function(){
-    try{
-        console.log("Server Start port 5000")
-        await connect()
-
-    }
-    catch(err){
-        console.log(err.message)
-    }
-})
+app.listen(5000, async () => {
+  try {
+    console.log("Server Start port 5000");
+    await connect();
+  } catch (err) {
+    console.log(err.message);
+  }
+});
